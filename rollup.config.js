6ced13546141f2841677ffe0765adc78dab82f2a
panmingming <panmingming@threegene.com>
2022-01-19 14:41:43 +0800

    初始化

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commomjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import terser from "rollup-plugin-terser";
+import cleaner from "rollup-plugin-cleaner";
+
+const env = process.env.NODE_ENV;
+
+const config = {
+
+};
+
+exports.module = config;
+
